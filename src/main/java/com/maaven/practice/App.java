15b6b452d96dae928ef77388e76a8ef3e0c6a90b
ramanjiazuredevops <89173707+ramanjiazuredevops@users.noreply.github.com>
2022-09-19 20:34:39 +0530

    Update App.java

--- src/main/java/com/maaven/practice/App.java
+++ src/main/java/com/maaven/practice/App.java
@@ -4,7 +4,7 @@ package com.maaven.practice;
  * Hello world!
  *
  */
-public class App 
+pubsssslic class App 
 {
     public static void main( String[] args )
     public static void main( String[] args )
